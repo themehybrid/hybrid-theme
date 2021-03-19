f99017e5efb2aa8c521634e26d25da09ca464018
saasfreelancer <syed.sayal.shah@gmail.com>
2021-03-19 17:55:01 +0500

    Cleanup code, remove un-necessary file loading.

--- src/bootstrap-theme.php
+++ src/bootstrap-theme.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Framwork bootstrap process.
+ * Functions files bootstrap.
  *
- * This file bootstraps parts of the framework that can't be autoloaded. We
- * define any global constants here and load any additional functions files.
+ * This file loads all of our functions files necessary for using the framework.
+ * Note that we are not loading class files.  Those are loaded via the autoloader
+ * in `bootstrap-autoload.php`.
  *
  * @package   HybridCore
  * @author    Justin Tadlock <justintadlock@gmail.com>
@@ -12,4 +13,12 @@
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-require_once( 'bootstrap-functions.php' );
+# Autoloads our custom functions files that are not loaded via the class loader.
+require_once( 'functions-filters.php'         );
+require_once( 'functions-helpers.php'         );
+require_once( 'functions-theme.php'           );
+require_once( 'Comment/functions-comment.php' );
+require_once( 'Menu/functions-menu.php'       );
+require_once( 'Post/functions-post.php'       );
+require_once( 'Sidebar/functions-sidebar.php' );
+require_once( 'Site/functions-site.php'       );
